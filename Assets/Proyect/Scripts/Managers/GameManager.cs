ee22ed17e288d00be3c5a3daf1399fbc6cfc1fa7
Carlos Alejandro <alexco_99@hotmail.com>
2025-08-07 01:30:20 -0600

    Add GameManager script and initial project folders
    Created the GameManager.cs script to manage game state and flow, implementing a singleton pattern. Added initial folder structure for Animations, Prefabs, Scripts (including Managers and Player), and Sounds, along with their corresponding Unity .meta files.

---
+++ Assets/Proyect/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This script is responsible for managing the game state and flow
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        // Ensure that there is only one instance of GameManager
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Keep this instance across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicate instances
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
